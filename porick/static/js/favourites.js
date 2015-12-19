4762e726fcb57eab3d861d745ac7858e94d6a2ca
Aengus Walton <Aengus.Walton@blue-yonder.com>
2015-12-19 18:38:03 +0000

    Fix route

--- porick/static/js/favourites.js
+++ porick/static/js/favourites.js
@@ -2,7 +2,7 @@ function setupFavouritesClickHandlers() {
     /**
      * Assign click handlers to all favourite buttons.
      */
-    $('.favourite').click(function() { 
+    $('.favourite').click(function() {
     	var quote_id = $(this).data('quote_id');
         if($(this).hasClass('favourited')) {
         	removeFavourite(quote_id, $(this));
@@ -14,7 +14,7 @@ function setupFavouritesClickHandlers() {
 
 function removeFavourite(quote_id, button) {
 	$.ajax({
-        url: '/api/v1/quotes'+quote_id+'favourite',
+        url: '/api/v1/quotes/'+quote_id+'/favourite',
         type: 'DELETE',
         success: function(data, status, jqXHR){
             button.removeClass('success error favourited');
